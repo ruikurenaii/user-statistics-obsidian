5bc6221fe9b0808148bde6c31548caea3fc0d9ea
Louie Soulen Kurenai <168345503+ruikurenaii@users.noreply.github.com>
2025-04-22 20:53:01 +0800

    Update and rename main.ts to src/main.ts

---
+++ src/main.ts
@@ -0,0 +1,7 @@
+import { App, Plugin } from 'obsidian';
+
+export default class UserStatsPlugin extends Plugin {
+	async onload() {
+		// there's nothing to see here yet, will plan onto adding some things as soon as possible ;)
+	}
+}
