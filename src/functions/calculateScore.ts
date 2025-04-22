a3282ccc780273021146c8332146dce886c5de89
Louie Soulen Kurenai <168345503+ruikurenaii@users.noreply.github.com>
2025-04-23 07:17:29 +0800

    fix typo on calculateScore.ts

--- src/functions/calculateScore.ts
+++ src/functions/calculateScore.ts
@@ -5,6 +5,6 @@ export function calculateScore(a): Promise<number> {
   const totalCharacters = await calculateCharacters(this.app);
   const totalFocusTime = this.settings.totalFocusTime ?? 0;
 
-  let score: number = (total characters * 10.0932743731) + (totalFocusTime / 1000);
+  let score: number = (totalCharacters * 10.0932743731) + (totalFocusTime / 1000);
   return score;
 }
